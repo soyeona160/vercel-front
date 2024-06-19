5ef25f4903759273f94261526950829abcde61a4
Yeona <soyeona160@gmail.com>
2024-06-19 14:06:43 +0900

    '회원가입로직'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import dotenv from 'dotenv'
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    console.log('서버주소: ', process.env.REACT_APP_BASE_URL)
+
+    fetch(`${process.env.REACT_APP_BASE_URL}/user`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            name: "react",
+            email: "react@gmail.com",
+            userId: "react",
+            password: "react123@"  
+        })
+    })
+    .then(res => res.json())
+    .then(result => {
+      console.log(result)
+      setUser(result.newUser)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <>
+          <h1>회원정보</h1>
+          <p>이름: {user.name}</p>
+          <p>연락처: {user.email}</p>
+          <p>아이디: {user.userId}</p>
+        </>
+      ) : "사용자정보 조회중..."}
     </div>
   );
 }
